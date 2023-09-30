df999f3f1fb545a03f1ed5368edf4658bb4156d6
lancechise <lancechise@icloud.com>
2023-09-30 09:43:00 -0400

    added comments

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
- 
-        document.getElementById("generate").addEventListener("click", function () {
-          const passwordLength = document.getElementById("passwordLength").value;
-          const includeUppercase = document.getElementById("includeUppercase").checked;
-          const includeNumbers = document.getElementById("includeNumbers").checked;
-          const includeSpecialChars = document.getElementById("includeSpecialChars").checked;
 
-          const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-          const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          const numberChars = "0123456789";
-          const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+function generatePassword() {
+    // Get password length from user input
+    const passwordLength = document.getElementById("passwordLength").value;
 
-          let chars = lowercaseChars;
-          if (includeUppercase) chars += uppercaseChars;
-          if (includeNumbers) chars += numberChars;
-          if (includeSpecialChars) chars += specialChars;
+    // Get user preferences for password characters
+    const includeUppercase = document.getElementById("includeUppercase").checked;
+    const includeNumbers = document.getElementById("includeNumbers").checked;
+    const includeSpecialChars = document.getElementById("includeSpecialChars").checked;
 
-          let password = "";
-          for (let i = 0; i < passwordLength; i++) {
-              const randomIndex = Math.floor(Math.random() * chars.length);
-              password += chars.charAt(randomIndex);
-          }
+    // Define character sets for each type of character
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
 
-          document.getElementById("password").textContent = password;
-      });
-  
+    // Combine character sets based on user preferences
+    let chars = lowercaseChars;
+    if (includeUppercase) chars += uppercaseChars;
+    if (includeNumbers) chars += numberChars;
+    if (includeSpecialChars) chars += specialChars;
+
+    // Generate password by randomly selecting characters from the combined set
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars.charAt(randomIndex);
+    }
+
+    // Display generated password in the password field
+    document.getElementById("password").textContent = password;
+}
